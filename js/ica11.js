464d74ebf84a2485c3870ec93b8f3b09a5b6261d
Emma Moore <etmisg@byu.edu>
2024-12-13 15:16:51 -0700

    qty, prce,

--- js/ica11.js
+++ js/ica11.js
@@ -2,21 +2,32 @@
 let groceryItems = [];
 
 const foodInput = document.getElementById('food-input');
+const quantityInput = document.getElementById('quantity-input');
+const priceInput = document.getElementById('price-input');
+const categoryInput = document.getElementById('category-input');
 const addFoodButton = document.getElementById('add-food-button');
 const groceryList = document.getElementById('grocery-list');
 
 addFoodButton.addEventListener('click', () => {
+
   const food = foodInput.value.trim();
-  if (food) {
+  const quantity = quantityInput.value.trim();
+  const price = priceInput.value.trim();
+  const category = categoryInput.value;
+
+  if (food && quantity && price && category) {
     const listItem = document.createElement('li');
-    listItem.textContent = food;
+    listItem.textContent = `${food}, ${quantity}, $${price}, ${category}`;
 
     groceryList.appendChild(listItem);
 
-    console.log(`Food added: ${food}`);
+    console.log(`Added item: ${food}, ${quantity}, $${price}, ${category}`);
 
     foodInput.value = '';
+    quantityInput.value = '';
+    priceInput.value = '';
+    categoryInput.value = 'produce';
   } else {
-    alert('Please enter a food item.');
+    alert('Please fill in all fields to add an item.');
   }
 });
